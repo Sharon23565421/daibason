40b6299b97762146ba415bdfa5b6cefb7f077109
kosame <Sharon23565421@gmail.com>
2023-09-06 02:00:59 +0800

    0905/卡片更新、新增文字行高

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,11 +22,13 @@
     </div>
     <div class="text">
       <div class="title">
-        <h4>五分埔商圈購物</h4>
-        <p class="s_text">台北成衣街｜衣服、飾品、配件、包包、帽子批發</p>
-        <p class="s_text time">2020-06-15</p>
-        <span class="title-tag">#景點推薦</span>
-        <span class="title-tag blue">#板南線</span>
+        <h3>五分埔商圈購物</h3>
+        <p>台北成衣街｜衣服、飾品、配件、包包、帽子批發</p>
+        <div class="tag">
+          <span class="title-tag gray">#景點推薦</span>
+          <span class="title-tag blue">#板南線</span>
+        </div>
+        <span class="s_text time">2020-06-15</span>
       </div>
       <div class="txt">
         　　那天逛完華山KAKAO敗家後，就心血來潮到五分埔去逛逛，真的是久違了好多年，以前學生時期過年領完紅包，一定都是最期待來到五分埔買衣服！如今後來才知道原來這裡除了一般民眾、觀光客買購物衣服的地方以外，原來也有成衣批發商圈，那天去晃晃除了衣服之外，還有包包、飾品、鞋子等等。
@@ -35,22 +37,24 @@
     </div>
   </div>
 
-  <div class="card-h">
+  <div class="card-h border-r">
     <div class="img">
       <img src="https://picsum.photos/280/200/?random=10" />
     </div>
     <div class="text">
       <div class="title">
-        <h4>原來京站樓上長這樣？</h4>
-        <p class="s_text">開箱五星級行政豪華客房一泊二食</p>
-        <p class="s_text time">2020-06-15</p>
-        <span class="title-tag">#住宿推薦</span>
-        <span class="title-tag blue">#板南線</span>
-        <span class="title-tag red">#淡水信義線</span>
+        <h3>原來京站樓上長這樣？</h3>
+        <p>開箱五星級行政豪華客房一泊二食</p>
+        <div class="tag">
+          <span class="title-tag blue">#板南線</span>
+          <span class="title-tag red">#淡水信義線</span>
+          <span class="title-tag gray">#住宿推薦</span>
+        </div>
       </div>
       <div class="txt">
         　　那天逛完華山KAKAO敗家後，就心血來潮到五分埔去逛逛，真的是久違了好多年，以前學生時期過年領完紅包，一定都是最期待來到五分埔買衣服！如今後來才知道原來這裡除了一般民眾、觀光客買購物衣服的地方以外，原來也有成衣批發商圈，那天去晃晃除了衣服之外，還有包包、飾品、鞋子等等。
       </div>
+      <span class="s_text time">2020-06-15</span>
       <img src="https://picsum.photos/80/80/?random=10" class="head" />
     </div>
   </div>
@@ -62,33 +66,37 @@
     <div class="text">
       <div class="title">
         <h4>五分埔商圈購物</h4>
-        <p class="ss_text">台北成衣街｜衣服、飾品、配件、包包、帽子批發</p>
-        <p class="time ss_text">2020-06-15</p>
-        <span class="title-tag">#景點推薦</span>
-        <span class="title-tag blue">#板南線</span>
+        <p class="s_text">台北成衣街｜衣服、飾品、配件、包包、帽子批發</p>
+        <div class="tag">
+          <span class="title-tag gray">#景點推薦</span>
+          <span class="title-tag blue">#板南線</span>
+        </div>
+        <span class="time ss_text">2020-06-15</span>
       </div>
       <img src="https://picsum.photos/50/50/?random=10" class="head" />
     </div>
   </div>
 
   <div class="ChangAC">
-    <i class="fa-solid fa-lock" style="color: #000000;"></i>
+    <i class="fa-solid fa-lock" style="color: #000000"></i>
     <p>更改帳號/密碼</p>
   </div>
 
-  <div class="ChangAC ChangACHover ">
-    <i class="fa-solid fa-lock" style="color: #000000;"></i>
+  <div class="ChangAC ChangACHover">
+    <i class="fa-solid fa-lock" style="color: #000000"></i>
     <p>更改帳號/密碼</p>
   </div>
 
-  <div class="ChangAC ChangACActive ">
+  <div class="ChangAC ChangACActive">
     <i class="fa-solid fa-lock"></i>
     <p>更改帳號/密碼</p>
   </div>
 
   <div class="SliderAC">
     <div class="pic">
-      <img src="https://steam.oxxostudio.tw/webp/gimp/example/simple-keyer-mask-16.webp">
+      <img
+        src="https://steam.oxxostudio.tw/webp/gimp/example/simple-keyer-mask-16.webp"
+      />
     </div>
     <h3>NAME</h3>
     <button>登出</button>
