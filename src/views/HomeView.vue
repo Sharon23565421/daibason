d1dd40da56407b7a50fb39a316f3ab794a3539dd
mochi_nino <z898911z@gmail.com>
2023-09-08 21:17:01 +0800

    新新新

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,45 +6,57 @@
           <div class="pic">
             <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
           </div>
-          <router-link to="/mrt" class="mrt"> 捷運推薦 </router-link>
-        </li>
-        <li>
-          <div class="pic">
-            <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
-          </div>
-          <router-link to="/game" class="game"> 捷運冒險 </router-link>
-        </li>
-        <li>
-          <div class="pic">
-            <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
-          </div>
-          <router-link to="/contribute" class="contribute">
-            投稿專欄
+          <router-link to="/mrt" class="mrt">
+            <p>捷運推薦</p>
           </router-link>
         </li>
         <li>
           <div class="pic">
             <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
           </div>
-          <router-link to="/Login" class="login"> 會員中心 </router-link>
+          <router-link to="/game" class="game">
+            <p>捷運冒險</p>
+          </router-link>
         </li>
         <li>
           <div class="pic">
             <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
           </div>
-          <router-link to="/store" class="store"> 精選購物 </router-link>
+          <router-link to="/contribute" class="contribute">
+            <p>投稿專欄</p>
+          </router-link>
         </li>
         <li>
           <div class="pic">
             <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
           </div>
-          <router-link to="/newView" class="new"> 最新消息 </router-link>
+          <router-link to="/Login" class="login">
+            <p>會員中心</p>
+          </router-link>
         </li>
         <li>
           <div class="pic">
             <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
           </div>
-          <router-link to="/about" class="about"> 關於我們 </router-link>
+          <router-link to="/store" class="store">
+            <p>精選購物</p>
+          </router-link>
+        </li>
+        <li>
+          <div class="pic">
+            <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
+          </div>
+          <router-link to="/newView" class="new">
+            <p>最新消息</p>
+          </router-link>
+        </li>
+        <li>
+          <div class="pic">
+            <img src="https://picsum.photos/1200/1000/?random=1" alt="" />
+          </div>
+          <router-link to="/about" class="about">
+            <p>關於我們</p>
+          </router-link>
         </li>
       </ul>
     </nav>
@@ -73,7 +85,7 @@
         </div>
       </div>
       <div class="logo">
-        <img src="../../public/img/logo.png" alt="logo" />
+        <img src="@/img/logo.png" alt="logo" />
       </div>
       <div class="swiper">
         <div class="swiper-wrapper">
@@ -98,15 +110,33 @@
         </div>
       </div>
     </div>
+
+    <!-- 元件展示 -->
+
+    <div class="ChangAC">
+      <i class="fa-solid fa-lock"></i>
+      <p>更改帳號/密碼</p>
+    </div>
+
+    <div class="SliderAC">
+      <div class="pic">
+        <img src="https://steam.oxxostudio.tw/webp/gimp/example/simple-keyer-mask-16.webp" />
+      </div>
+      <h3>NAME</h3>
+      <button>登出</button>
+      <nav>
+        <ul>
+          <li><a href="">會員資料</a></li>
+          <li><a href="">投稿文章</a></li>
+          <li><a href="">文章收藏</a></li>
+          <li><a href="">商品收藏</a></li>
+          <li><a href="">訂單查詢</a></li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.container {
-  // height: 100vh;
-  // overflow: hidden;
-}
-</style>
 
 <script>
 import Swiper from "swiper/bundle";
@@ -125,15 +155,14 @@ export default {
         disableOnInteraction: false,
       },
     });
+
   },
 };
 
-window.addEventListener("load", function () {
-  // 获取当前视口宽度和高度
-  const viewportWidth = window.innerWidth;
-  const viewportHeight = window.innerHeight;
-  console.log(viewportWidth);
 
-  // 根据需要进行调整页面元素的大小和布局
-});
+
+
+
+
+
 </script>
