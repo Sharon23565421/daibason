7d8048475f4c869591879d14d0c862ecf110be56
minyuan <a1097562@gmail.com>
2023-09-10 00:18:15 +0800

    最新消息v3

--- src/views/newView.vue
+++ src/views/newView.vue
@@ -2,40 +2,48 @@
   <div class="newsWrap">
     <h1>最新消息</h1>
     <div class="newsBanner">
-      <!-- 背景模糊 -->
 
-      <!-- 輪播區塊 -->
-      <div class="newsSlider">
+      <div class="swiper">
+        <!-- 輪播區塊 -->
+        <div class="newsSlider swiper-wrapper">
 
-        <div class="newsCtx" v-for="news in slider">
+          <div class="newsCtx swiper-slide" v-for="news in slider">
 
-          <div class="newsBackpic">
-            <img :src="news.image" alt="">
-          </div>
-
-          <div class="newsCtxTxt">
-            <div class="newsTag">
-              <span>{{ news.tag }}</span>
+            <!-- 背景模糊 -->
+            <div class="newsBackpic">
+              <img :src="news.image" alt="">
             </div>
-            <div class="newsDate">
-              {{ news.date }}
+            <div class="newsBox">
+              <div class="newsCtxTxt">
+                <div class="newsTag">
+                  <span>{{ news.tag }}</span>
+                </div>
+                <div class="newsDate">
+                  {{ news.date }}
+                </div>
+                <h2>{{ news.title }}</h2>
+                <h3>{{ news.title2 }}</h3>
+                <button>更多</button>
+              </div>
+
+              <div class="newsCtxPic">
+                <img :src="news.image" alt="">
+              </div>
+
             </div>
-            <h2>{{ news.title }}</h2>
-            <h3>{{ news.title2 }}</h3>
-            <button>更多</button>
           </div>
 
-          <div class="newsCtxPic">
-            <img :src="news.image" alt="">
-          </div>
         </div>
 
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import Swiper from "swiper/bundle";
+
 export default {
   data() {
     return {
@@ -54,6 +62,21 @@ export default {
         image: '../img/newsbackimg2.png'
       }]
     }
+  }, 
+  mounted() {
+    const swiper = new Swiper(".swiper", {
+      // Optional parameters
+      direction: "horizontal",
+      speed: 3000,
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      // autoplay: {
+      //   delay: 0,
+      //   pauseOnMouseEnter: true,
+      //   disableOnInteraction: false,
+      // },
+    });
   },
 }
 </script>
