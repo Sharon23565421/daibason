1dd884643f639b16618266b5628fd990f2ddba5c
mochi_nino <z898911z@gmail.com>
2023-09-10 16:57:52 +0800

    components

---
+++ src/components/SliderAC.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="SliderAC">
+        <div class="pic">
+            <img src="https://steam.oxxostudio.tw/webp/gimp/example/simple-keyer-mask-16.webp" />
+        </div>
+        <h3>NAME</h3>
+        <button>登出</button>
+        <nav>
+            <ul>
+                <li><router-link to="">會員資料</router-link></li>
+                <li><router-link to="">投稿文章</router-link></li>
+                <li><router-link to="">文章收藏</router-link></li>
+                <li><router-link to="">商品收藏</router-link></li>
+                <li><router-link to="">訂單查詢</router-link></li>
+            </ul>
+        </nav>
+    </div>
+</template>
--- src/views/mrtView.vue
+++ src/views/mrtView.vue
@@ -1,7 +1,17 @@
 <template>
-
   <div class="about">
     <h1>This is an MRT page</h1>
   </div>
 
+<SliderAC/>
 </template>
+
+
+<script>
+import SliderAC from '@/components/SliderAC.vue';
+export default {
+  components: {
+    SliderAC,
+  }
+}
+</script>
